9c0a4adefa0b6cad491aef32b4122efebe64f2d5
Gustavo Lima <gustavo.lima@nd.com.br>
2024-01-23 15:16:47 -0300

    feat

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,5 @@
+import { test } from 'vitest'
+// @vitest-environment vitest-enviroment-prisma
+
+// eslint-disable-next-line prettier/prettier, @typescript-eslint/no-empty-function
+test('ok', () => { })
---
+++ vitest-enviroment-prisma/index.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL enviroment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+export default <Environment>(<unknown>{
+  name: 'prisma',
+  transformMode: 'web',
+  async setup() {
+    const schema = randomUUID()
+
+    const databaseUrl = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma migrate deploy')
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
+      },
+    }
+  },
+})
