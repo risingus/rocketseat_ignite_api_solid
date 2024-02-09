80e771377ffcdf034cc8a48a7904253ac15ff419
Gustavo Lima <gustavo.it@outlook.com>
2024-02-09 16:38:09 -0300

    feat

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { create } from './create'
+import { validate } from './validate'
+import { history } from './history'
+import { metrics } from './metrics'
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metrics)
+  app.post('/gyms/:gymid/check-ins', create)
+  app.patch('/check-ins/:checkInId/validate', validate)
+}
---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,56 @@
+// @vitest-environment vitest-enviroment-prisma
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gyms by title', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gym',
+        description: 'Some description',
+        phone: '8787878787',
+        latitude: -3.8251901,
+        longitude: -38.4248003,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Typescript Gym',
+        description: 'Some description',
+        phone: '8787878787',
+        latitude: -3.8251901,
+        longitude: -38.4248003,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'Javascript',
+      })
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toEqual(201)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Javascript Gym',
+      }),
+    ])
+  })
+})
